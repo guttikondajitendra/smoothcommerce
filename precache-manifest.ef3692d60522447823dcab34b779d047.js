e1129890d004b690bf8cacb823e29dd18bba9606
JitendraGuttikonda <guttikondajitendra@gmail.com>
2020-10-20 12:38:10 -0400

    Updates

---
+++ precache-manifest.ef3692d60522447823dcab34b779d047.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e31a15661d11c193b1b22607dd5620a",
+    "url": "/smoothcommerce/index.html"
+  },
+  {
+    "revision": "9c8d86bf67a1b2bfca46",
+    "url": "/smoothcommerce/static/css/main.4e362ecd.chunk.css"
+  },
+  {
+    "revision": "c28a6444f22751ee183b",
+    "url": "/smoothcommerce/static/js/2.f2a96944.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/smoothcommerce/static/js/2.f2a96944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c8d86bf67a1b2bfca46",
+    "url": "/smoothcommerce/static/js/main.f359bcff.chunk.js"
+  },
+  {
+    "revision": "b13c3715a65faeb4dea0",
+    "url": "/smoothcommerce/static/js/runtime-main.7830afac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smoothcommerce/static/media/logo.5d5d9eef.svg"
+  }
+]);
